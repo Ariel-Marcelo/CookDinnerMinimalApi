d9a4e5cfea86c9b3c61cfb68736df2fc0c91f478
Ariel Marcelo <55715664+Ariel-Marcelo@users.noreply.github.com>
2024-11-03 15:29:24 -0500

    ci: fix warnings Recipe posible null variables

--- CookDinnerMinimalApi/Models/Recipe.cs
+++ CookDinnerMinimalApi/Models/Recipe.cs
@@ -4,9 +4,9 @@ public class Recipe
 {
     public Guid Id { get; set; }
     
-    public string Name { get; set; }
+    public string? Name { get; set; }
     
-    public List<string> Ingredients { get; set; }
+    public List<string>? Ingredients { get; set; }
     
     public int PreparationTime { get; set; }
     
