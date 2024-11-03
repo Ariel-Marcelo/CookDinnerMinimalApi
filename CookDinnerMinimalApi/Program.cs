52d266c118691182de8e9c11fb73bca3bd21ff2a
Ariel Marcelo <55715664+Ariel-Marcelo@users.noreply.github.com>
2024-11-03 15:31:04 -0500

    test: no set kestrel notification

--- CookDinnerMinimalApi/Program.cs
+++ CookDinnerMinimalApi/Program.cs
@@ -4,10 +4,11 @@ using CookDinnerMinimalApi.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Use Kestrel
+/*
 if (Environment.GetEnvironmentVariable("USE_KESTREL") == "true")
 {
     builder.WebHost.UseKestrel();
-}
+}*/
 
 // Dependency Inyection
 builder.Services.AddSingleton<IRecipeRepository, RecipeRepository>();
